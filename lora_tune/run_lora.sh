9edd7e7d12d1f325c9e8e6207916ce6b4fee78ae
Jackson Romero <jacksontromero@gmail.com>
2025-04-07 12:20:09 -0400

    basic lora working

--- lora_tune/run_lora.sh
+++ lora_tune/run_lora.sh
@@ -2,4 +2,4 @@
 
 tune download Qwen/Qwen2.5-0.5B --output-dir /tmp/Qwen2_5-0_5B-Base
 
-tune run lora_tune/recipe.py --config lora_tune/config.yaml --base-model-path /tmp/Qwen2_5-0_5B-Base
+tune run recipe.py --config config.yaml
