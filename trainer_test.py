0f2bf1c84ad3c8dfe7e40858744dc63cc1c94f9b
lizchu413 <lizchu413@gmail.com>
2025-04-07 12:49:31 -0400

    testing dataloader

--- trainer_test.py
+++ trainer_test.py
@@ -1,6 +1,9 @@
-from base_trainer import Trainer
+import datasets
+
+from fast_trainer import Trainer
 from transformers import (AutoModelForSequenceClassification, 
-                          AutoModelForSeq2SeqLM, 
+                          AutoModelForSeq2SeqLM,
+                          AutoModelForCausalLM,
                           AutoTokenizer,
                           TrainingArguments)
 from datasets import load_dataset
@@ -49,6 +52,7 @@ def test_dataload_2():
 
     # tokenize and subset data.
     small_train_dataset = dataset["train"].shuffle(seed=42).select(range(100))
+    print(small_train_dataset[0])
     small_train_dataset = small_train_dataset.map(tokenize_function, batched=True)
     small_eval_dataset = dataset["train"].shuffle(seed=42).select(range(100))
     small_eval_dataset = small_eval_dataset.map(tokenize_function, batched=True)
@@ -64,6 +68,46 @@ def test_dataload_2():
     assert(len(next(iter(test_dataloader))['input_ids']) == training_args.eval_batch_size)
 
 
+def test_codexglue_dataload():
+    java_ds = load_dataset("google/code_x_glue_cc_code_completion_token", "java")
+    python_ds = load_dataset("google/code_x_glue_cc_code_completion_token", "python")
+
+    # also testing model?
+    model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-0.5B")
+    tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-0.5B")
+
+    def tokenize_function(examples):
+        code_strs = [" ".join(code_lst) for code_lst in examples["code"]]
+        tokens = tokenizer(code_strs, padding="max_length", truncation=True, max_length=512)
+        tokens["labels"] = tokens["input_ids"].copy()
+        return tokens
+
+    # tokenize, subset data
+    small_java_train = java_ds["train"].shuffle(seed=42).select(range(10))
+    small_python_train = python_ds["train"].shuffle(seed=42).select(range(10))
+
+    small_java_eval = java_ds["validation"].shuffle(seed=42).select(range(10))
+    small_python_eval = python_ds["test"].shuffle(seed=42).select(range(10)) # so the python dataset does Not Have Val
+
+    combine_train = datasets.concatenate_datasets([small_java_train, small_python_train])
+    combine_eval = datasets.concatenate_datasets([small_java_eval, small_python_eval])
+
+    combine_train = combine_train.map(tokenize_function, batched=True)
+    combine_eval = combine_eval.map(tokenize_function, batched=True)
+
+    print(combine_train[0])
+    print(f"len of code: {len(combine_train[0]['code'])} len of tokens: {len(combine_train[0]['input_ids'])}")
+
+    training_args = TrainingArguments(
+        output_dir="test_trainer",
+    )
+    trainer = Trainer(model=model, args=training_args, train_dataset=combine_train, eval_dataset=combine_eval)
+
+    train_dataloader = trainer.get_train_dataloader()
+    trainer.train()
+
+
 if __name__ == "__main__":
-    test_dataload_1()
-    test_dataload_2()
+    # test_dataload_1()
+    # test_dataload_2()
+    test_codexglue_dataload()
