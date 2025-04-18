83e8a9d023ea469dbce123dc5431a84dacbf39b2
Jackson Romero <jacksontromero@gmail.com>
2025-04-18 01:55:54 -0400

    attempted fixes

--- tuning/lora_tune/debug_run_lora.sh
+++ tuning/lora_tune/debug_run_lora.sh
@@ -4,4 +4,4 @@ export PYTHONPATH=$PYTHONPATH:../..
 
 tune download Qwen/Qwen2.5-0.5B --output-dir /tmp/Qwen2_5-0_5B-Base
 
-tune run tuning/custom_recipes/fim/fim_lora_recipe.py --config debug_config.yaml
+tune run tuning/custom_recipes/fim/fim_adapter_recipe.py --config debug_config.yaml
