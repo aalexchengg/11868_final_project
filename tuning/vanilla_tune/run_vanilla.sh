b77697c577de777080f155674314ef178dcf20ba
lizchu413 <lizchu413@gmail.com>
2025-04-22 19:18:33 -0400

    vanilla changes

--- tuning/vanilla_tune/run_vanilla.sh
+++ tuning/vanilla_tune/run_vanilla.sh
@@ -5,5 +5,5 @@ export PYTHONPATH=$PYTHONPATH:../..
 tune download Qwen/Qwen2.5-0.5B --output-dir /tmp/Qwen2_5-0_5B-Base
 #tune download Qwen/Qwen2.5-1.5B --output-dir /tmp/Qwen2_5-1_5B-Base
 
-tune run recipe.py --config config_0_5.yaml > phase1_csn.txt
+tune run recipe.py --config config_0_5_csn.yaml > phase1_csn.txt
 #tune run recipe.py --config config_1_5.yaml
