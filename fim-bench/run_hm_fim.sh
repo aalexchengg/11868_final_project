b89ed5bf7871e2fdf71b540f1b2720e715b8253f
lizchu413 <lizchu413@gmail.com>
2025-04-23 09:37:47 -0400

    typo

--- fim-bench/run_hm_fim.sh
+++ fim-bench/run_hm_fim.sh
@@ -1,11 +1,11 @@
 export LC_ALL="POSIX"
 
 INPUT_MODEL=$1
-OUTPUT_DIR=$2/humaneval-infilling
+OUTPUT_DIR=$2
 TP=$3
 
 
-mkdir -p ${OUTPUT_DIR}/humaneval-infilling
+mkdir -p $2/humaneval-infilling
 python hm_fim/humaneval_fim.py \
     --model_type codelm_leftright_context \
     --model_name_or_path ${INPUT_MODEL} \
