f6ac2cd40e3e3f846ad36b605d860f73480cc53b
Jackson Romero <jacksontromero@gmail.com>
2025-04-23 10:50:01 -0400

    phase 3

---
+++ tuning/phase_3/dumb_csn/run_lora_ff.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PYTHONPATH=$PYTHONPATH:../..
+
+CONFIG_OPTION=$1
+
+# download base model
+tune download jtromero/qwen2-0.5b-phase2-codexglue-lora-ff --output-dir /local/Qwen2_5-0_5B-Phase2
+
+tune run tuning/custom_recipes/fim/fim_adapter_recipe.py --config ${CONFIG_OPTION}
