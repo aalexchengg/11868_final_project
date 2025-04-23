1516ef6e2177903907108a7945d7449c459a1c7c
lizchu413 <lizchu413@gmail.com>
2025-04-23 09:33:24 -0400

    hm_fim changes

--- fim-bench/run_hm_fim.sh
+++ fim-bench/run_hm_fim.sh
@@ -9,5 +9,9 @@ mkdir -p ${OUTPUT_DIR}/humaneval-infilling
 python hm_fim/humaneval_fim.py \
     --model_type codelm_leftright_context \
     --model_name_or_path ${INPUT_MODEL} \
+    --right_context_length 2048 \
+    --input_file ./hm_fim/data/fim_singleline.jsonl \
+    --gen_length 50 \
+    --max_seq_length 8192 \
     --output_dir ${OUTPUT_DIR} \
     --tp ${TP}
