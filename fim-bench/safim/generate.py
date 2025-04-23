b1e3684e4d4bb52a399c161533441143fb54ed56
lizchu413 <lizchu413@gmail.com>
2025-04-23 10:51:24 -0400

    safim folder

---
+++ fim-bench/safim/generate.py
@@ -0,0 +1,80 @@
+import argparse
+import json
+import os
+from typing import Iterable
+
+from tqdm import tqdm
+
+from data_utils import load_dataset
+from model_utils import ModelWrapper, build_model
+from prompt_utils import apply_postprocessors, apply_prompt
+
+
+def handle_example(
+    sample: dict,
+    completion_type: str,
+    mode: str,
+    model_wrapper: ModelWrapper,
+    cache: dict,
+    post_processors: Iterable[str]
+):
+    prompt = apply_prompt(sample, completion_type, mode, model_wrapper)
+    completion = model_wrapper.invoke_cached(prompt, cache)
+    completion = apply_postprocessors(completion, sample, completion_type, post_processors)
+    return {
+        "task_id": sample["task_id"],
+        "completion": completion
+    }
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model_name", type=str)
+    parser.add_argument("completion_type", type=str)
+    parser.add_argument("cache_path", type=str)
+    parser.add_argument("output_path", type=str)
+    parser.add_argument("mode", type=str)
+    parser.add_argument("--block_comments", action="store_true")
+    parser.add_argument("--post_processors", type=str, nargs="+")
+
+    args = parser.parse_args()
+
+    if os.path.exists(args.cache_path):
+        with open(args.cache_path, "r", encoding="utf-8") as f:
+            cache = json.load(f)
+    else:
+        cache = {}
+
+    model_wrapper = build_model(args)
+
+    outputs = []
+    try:
+        for sample in tqdm(load_dataset(args.completion_type)):
+            outputs.append(
+                handle_example(sample, args.completion_type, args.mode, model_wrapper, cache, args.post_processors)
+            )
+            if len(outputs) == 10:
+                for o in outputs:
+                    print("=====", o["task_id"], "=====")
+                    print(o["completion"])
+                    print("====================")
+                    print()
+            if len(outputs) % 1000 == 0:
+                with open(args.cache_path, "w", encoding="utf-8") as f:
+                    json.dump(cache, f)
+        success = True
+    except Exception as e:
+        print(e)
+        import traceback
+        traceback.print_exc()
+        success = False
+    with open(args.cache_path, "w", encoding="utf-8") as f:
+        json.dump(cache, f)
+    if success:
+        with open(args.output_path, "w", encoding="utf-8") as f:
+            for item in outputs:
+                f.write(json.dumps(item) + "\n")
+
+
+if __name__ == '__main__':
+    main()
---
+++ fim-bench/safim/show_detailed_results.py
@@ -0,0 +1,55 @@
+import argparse
+import csv
+import os
+import sys
+from collections import defaultdict
+
+from data_utils import load_dataset, stream_jsonl
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("completion_type", type=str)
+    parser.add_argument("result_path", type=str)
+    args = parser.parse_args()
+
+    problems = load_dataset(args.completion_type)
+    problem_to_lang = {problem["task_id"]: problem["lang"] for problem in problems}
+
+    pass_cnt, total = defaultdict(int), defaultdict(int)
+    results_cnt = defaultdict(int)
+    for r in stream_jsonl(args.result_path):
+        if 'passed' not in r:
+            print(r)
+        lang = problem_to_lang[r['task_id']]
+        pass_cnt[lang] += int(r['passed'])
+        total[lang] += 1
+        pass_cnt["all"] += int(r['passed'])
+        total["all"] += 1
+        if not r['passed']:
+            r_set = set()
+            if isinstance(r['result'], str):
+                if r['result'] != "EXACT_MATCH":
+                    r_set.add(r['result'])
+            else:
+                for rr in r['result']:
+                    if rr["exec_outcome"] != "PASSED":
+                        r_set.add(rr["exec_outcome"])
+            if len(r_set) > 1:
+                results_cnt["MIXED"] += 1
+            else:
+                results_cnt[list(r_set)[0]] += 1
+
+    ALL_OUTCOMES = ['EMPTY', 'COMPILATION_ERROR', 'RUNTIME_ERROR', 'MEMORY_LIMIT_EXCEEDED', 'TIME_LIMIT_EXCEEDED',
+                    'WRONG_ANSWER', 'MIXED']
+    writer = csv.writer(sys.stdout)
+    # writer.writerow(['C++', 'Java', 'Python', 'C#', 'All'] + ALL_OUTCOMES)
+    writer.writerow(
+        [os.path.splitext(os.path.basename(args.result_path))[0]]
+        + [f"{pass_cnt[lang] / total[lang] * 100:.6f}" for lang in ['cpp', 'java', 'python', 'csharp', 'all']]
+        + [str(results_cnt[outcome]) for outcome in ALL_OUTCOMES]
+    )
+
+
+if __name__ == '__main__':
+    main()
