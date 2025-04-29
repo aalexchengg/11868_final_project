8b57ff03c29191376d7c49f17ffe9187424436e7
Jackson Romero <jacksontromero@gmail.com>
2025-04-28 21:06:52 -0400

    final full run config

--- tuning/phase_3/dumb_csn/run_lora_ff.sh
+++ tuning/phase_3/dumb_csn/run_lora_ff.sh
@@ -6,7 +6,7 @@ CONFIG_OPTION=$1
 
 # download base model
 # tune download jtromero/qwen2-0.5b-phase2-codexglue-lora-ff --output-dir /local/Qwen2_5-0_5B-Phase2
-tune download lizchu413/phase1_qwen2.5_0.5b_gh --output-dir /local/Qwen2_5-0_5B-Phase1-GH
+tune download Qwen/Qwen2.5-0.5B --output-dir /local/Qwen2_5-0_5B-Base
 
 
 tune run tuning/custom_recipes/fim/fim_adapter_recipe.py --config ${CONFIG_OPTION}
