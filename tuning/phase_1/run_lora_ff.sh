3be5c5584ca9e674d82bffedacc7193967096da3
Jackson Romero <jacksontromero@gmail.com>
2025-04-28 21:13:53 -0400

    start final run from base

--- tuning/phase_1/run_lora_ff.sh
+++ tuning/phase_1/run_lora_ff.sh
@@ -6,6 +6,6 @@ export HF_DATASETS_CACHE="/local/cache/huggingface"
 CONFIG_OPTION=$1
 
 # download base model
-tune download jtromero/qwen2-0.5b-phase1-gh_plus-lora-ff --output-dir /local/Qwen2_5-0_5B-Phase1-GH-Checkpoint
+tune download Qwen/Qwen2.5-0.5B --output-dir /local/Qwen2_5-0_5B-Base
 
 tune run tuning/custom_recipes/fim/fim_adapter_recipe.py --config ${CONFIG_OPTION}
